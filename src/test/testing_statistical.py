61f22b2b33cacb0fd64ea3c6d86283c1f4e783fa
Massimiliano Christ <massi.omni@gmail.com>
2022-03-16 15:28:21 +0100

    fixed typo in testarray

--- src/test/testing_statistical.py
+++ src/test/testing_statistical.py
@@ -4,7 +4,7 @@ import statistical as mystat
 
 def test_euclidean_distance():
     """Test the euclidean distance function if it gets the values correct"""
-    testArray = np.array([2, 2, 2], [1, 1, 1], [3, 3, 3], [1, 1, 1])
+    testArray = np.array([[2, 2, 2], [1, 1, 1], [3, 3, 3], [1, 1, 1]])
 
     assert np.all(
         mystat.euclidean_norm(testArray, [0, 2], [1, 3]) == np.array([np.sqrt(3), 0])
