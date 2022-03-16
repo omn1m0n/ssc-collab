c4b5f14bbea14856d02151f0ca0aa2b4e8855141
Massimiliano Christ <massi.omni@gmail.com>
2022-03-16 15:36:53 +0100

    fixed math

--- src/test/testing_statistical.py
+++ src/test/testing_statistical.py
@@ -3,9 +3,10 @@ import statistical as mystat
 
 
 def test_euclidean_distance():
-    """Test the euclidean distance function if it gets the values correct"""
+    """Test the euclidean distance function if it gets simple values correct"""
     testArray = np.array([[2, 2, 2], [1, 1, 1], [3, 3, 3], [1, 1, 1]])
 
     assert np.all(
-        mystat.euclidean_norm(testArray, [0, 2], [1, 3]) == np.array([np.sqrt(3), 0])
+        mystat.euclidean_norm(testArray, [0, 2], [1, 3])
+        == np.array([np.sqrt(3), np.sqrt(12)])
     )
